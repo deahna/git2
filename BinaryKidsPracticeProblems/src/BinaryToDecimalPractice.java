df7ff9bc1781b5453f6573825ecc1f7130156ae6
deahna <drj31@georgetown.edu>
2015-04-27 04:36:25 -0400

    Updated, Basic changes.
    Won't run with what's in master brance so far.

--- BinaryKidsPracticeProblems/src/BinaryToDecimalPractice.java
+++ BinaryKidsPracticeProblems/src/BinaryToDecimalPractice.java
@@ -1,90 +1,208 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Random;
 
-import javax.swing.JApplet;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
 
 
-//the screen for converting binary numbers to decimal
+//the screen to practice converting decimal numbers to binary
 @SuppressWarnings("serial")
-public class BinaryToDecimalPractice extends JPanel implements GenerateProblem
+public class BinaryToDecimalPractice extends JPanel implements GenerateProblem , ActionListener
 {
+	protected JButton nextTutorialButton, previousTutorialButton, submitAnswerButton;
+	protected JLabel instructionLabel, successLabel, failureLabel; 
+	protected JTextArea editTextArea, typedAnswer;
+	
+	boolean correctAnswer = false;
+	String userResponse;
+	int convertValue;
 	
 	@Override
 	public void showQuestion(int articleQuestion) 
 	{
-		JLabel question = new JLabel("Convert this binary number to decimal.");
+		// TODO Auto-generated method stub
+		convertValue = initiateConvertValue();
+		
+		instructionLabel = new JLabel("Convert This Binary Number to Decimal! " + convertValue);
+		instructionLabel.setForeground(Color.WHITE);
+		instructionLabel.setVerticalTextPosition(JLabel.BOTTOM);
+		instructionLabel.setHorizontalTextPosition(JLabel.CENTER);
+		
+		submitAnswerButton = new JButton("Submit Answer!");
+	    submitAnswerButton.setVerticalTextPosition(AbstractButton.CENTER);
+	    submitAnswerButton.setHorizontalTextPosition(AbstractButton.LEADING);
+	    submitAnswerButton.setActionCommand("Submitted");
+	    submitAnswerButton.setToolTipText("Click this button to submit answer.");
+	    submitAnswerButton.addActionListener(this);
+	    
+	    add(submitAnswerButton);
+	    
+	    editTextArea = new JTextArea("Type Here");
+		editTextArea.setBackground(Color.BLUE);
+		editTextArea.setForeground(Color.WHITE);
+		
+		typedAnswer = new JTextArea();
+		typedAnswer.setEditable(false);
 		
 		
-		JTextField input = new JTextField();
-		JTextField answer = new JTextField();
+		add(instructionLabel);
+		add(editTextArea);
+		add(typedAnswer);
 		
 	}
 
-	//a member tasks that compares the user input with the correct answer
 	@Override
-	public boolean checkAnswer(long userAnswer, int articleQuestion) 
+	public boolean checkAnswer(String userAnswer, int articleQuestion) 
 	{
-		JLabel question = new JLabel("Correct Answer:");
-		return false;
+		int decimalNumber = articleQuestion;
+		int remainder;
+		String binary = "";
+				
+		while(decimalNumber > 0)
+		{
+			remainder = decimalNumber % 2;
+			binary = remainder + binary;
+			decimalNumber /= 2;
+		}
+		
+		if(userAnswer != binary)
+			return false;
+		return true;
 	}
 
-	//a directive to continue on in the program or to return to the previous tutorial
 	@Override
 	public void subsequentAction() 
 	{
 		// TODO Auto-generated method stub
 		
-	} 
+	}	
 	
-	public void init()
+	//initates the arraylist value with random decimal numbers
+	public int initiateConvertValue()
 	{
-		try
+		Random rand = new Random();
+		int chooseArticleQuestion = rand.nextInt(1000);
+		
+		return chooseArticleQuestion;			
+	}
+	
+	public BinaryToDecimalPractice()
+	{		
+		showQuestion(1);
+			    
+	    correctAnswer = checkAnswer(userResponse, convertValue);
+	    		
+		if(correctAnswer)
 		{
-			SwingUtilities.invokeAndWait(new Runnable()
-			{
-					public void run()
-					{
-						
-						
-						JPanel panel = new JPanel();
-						panel.setLayout(new BorderLayout());
-						panel.setName("Practice Calculating Binary to Decimal");
-					
-						JLabel lbl = new JLabel ("Practice Calculating Binary to Decimal");
-						
-						lbl.setFont(new Font("Verdana",23,30));
-						
-						
-						lbl.add(panel, BorderLayout.NORTH);
-						
-						lbl.setLocation(0, 20);
-						lbl.setSize(86, 14);
-						lbl.setBorder(new LineBorder(Color.BLUE));
-						
-						
-						add(lbl);
-					}
-		});
+			successLabel = new JLabel("Good Job! Proceed to the next tutorial!");
+				successLabel.setForeground(Color.WHITE);
+				successLabel.setVerticalTextPosition(JLabel.BOTTOM);
+				successLabel.setHorizontalTextPosition(JLabel.CENTER);
 			
-		}catch (Exception e)
+			nextTutorialButton = new JButton("Next Tutorial!");
+				nextTutorialButton.setVerticalTextPosition(AbstractButton.CENTER);
+				nextTutorialButton.setHorizontalTextPosition(AbstractButton.LEADING);
+				nextTutorialButton.setLocation(200, 100);
+				nextTutorialButton.setToolTipText("Click this button to proceed to next tutorial.");
+				nextTutorialButton.setActionCommand("Success");
+				nextTutorialButton.addActionListener(this);
+				
+				add(nextTutorialButton);
+		}
+		
+		else if(!correctAnswer)
+		{
+			failureLabel = new JLabel("Oops! You need more practice. Go back to the previous tutorial!");
+				failureLabel.setForeground(Color.WHITE);
+				failureLabel.setVerticalTextPosition(JLabel.BOTTOM);
+				failureLabel.setHorizontalTextPosition(JLabel.CENTER);
+			
+			previousTutorialButton = new JButton("Back to Tutorial!");
+				previousTutorialButton.setVerticalTextPosition(AbstractButton.BOTTOM);
+				previousTutorialButton.setHorizontalTextPosition(AbstractButton.CENTER);
+				nextTutorialButton.setLocation(200, 100);
+				previousTutorialButton.setToolTipText("Click this button to proceed to previous tutorial.");
+				previousTutorialButton.setActionCommand("Failure"); 
+				previousTutorialButton.addActionListener(this);
+				
+				add(previousTutorialButton);
+		}
+	       
+	}
+	
+	public static void initPracticeProblem()
+	{
+		//JPanel practicePanel = new JPanel();
+		//JLabel practiceLabel = new JLabel("Decimal to Binary Practice Problem.");
+		
+		JFrame frame = new JFrame("Binary to Decimal Practice Problem.");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		BinaryToDecimalPractice pane = new BinaryToDecimalPractice();
+		pane.setBackground(Color.BLACK);
+		//
+		pane.setOpaque(true);
+		pane.setPreferredSize(new Dimension(600, 500));
+		
+		//frame.setBackground(Color.BLUE);
+		frame.add(pane);
+		frame.setSize(500, 500);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+	}
+	
+	public static void main(String[] args)
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
+		{
+			public void run()
 			{
-				System.err.println("createGUI didn't complete successfully");
+				initPracticeProblem();
 			}
-			
+		});
 		
 	}
 
-
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		if("Success".equals(e.getActionCommand()))
+		{
+			//go to next tutorial
+		}
+		
+		else if("Failure".equals(e.getActionCommand()))
+		{
+			//go to previous tutorial
+		}
+		
+		else if("Submitted".equals(e.getActionCommand()))
+		{
+			userResponse = editTextArea.getText();
+			
+		}		
+		
+	}
 	
-
-
 }
--- BinaryKidsPracticeProblems/src/DecimalToBinaryPractice.java
+++ BinaryKidsPracticeProblems/src/DecimalToBinaryPractice.java
@@ -1,12 +1,20 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.LineBorder;
@@ -14,115 +22,187 @@ import javax.swing.border.LineBorder;
 
 //the screen to practice converting decimal numbers to binary
 @SuppressWarnings("serial")
-public class DecimalToBinaryPractice extends JPanel implements GenerateProblem
-{	
+public class DecimalToBinaryPractice extends JPanel implements GenerateProblem , ActionListener
+{
+	protected JButton nextTutorialButton, previousTutorialButton, submitAnswerButton;
+	protected JLabel instructionLabel, successLabel, failureLabel; 
+	protected JTextArea editTextArea, typedAnswer;
+	
+	boolean correctAnswer = false;
+	String userResponse;
+	int convertValue;
+	
 	@Override
 	public void showQuestion(int articleQuestion) 
 	{
-		JLabel question = new JLabel("Convert this decimal number to binary.");
+		// TODO Auto-generated method stub
+		convertValue = initiateConvertValue();
+		
+		instructionLabel = new JLabel("Convert This Decimal Number to Binary! " + convertValue);
+		instructionLabel.setForeground(Color.WHITE);
+		instructionLabel.setVerticalTextPosition(JLabel.BOTTOM);
+		instructionLabel.setHorizontalTextPosition(JLabel.CENTER);
+		
+		submitAnswerButton = new JButton("Submit Answer!");
+	    submitAnswerButton.setVerticalTextPosition(AbstractButton.CENTER);
+	    submitAnswerButton.setHorizontalTextPosition(AbstractButton.LEADING);
+	    submitAnswerButton.setActionCommand("Submitted");
+	    submitAnswerButton.setToolTipText("Click this button to submit answer.");
+	    submitAnswerButton.addActionListener(this);
+	    
+	    add(submitAnswerButton);
+	    
+	    editTextArea = new JTextArea("Type Here");
+		editTextArea.setBackground(Color.BLUE);
+		editTextArea.setForeground(Color.WHITE);
+		
+		typedAnswer = new JTextArea();
+		typedAnswer.setEditable(false);
 		
 		
-		JTextField input = new JTextField();
-		JTextField answer = new JTextField();
+		add(instructionLabel);
+		add(editTextArea);
+		add(typedAnswer);
 		
-				
 	}
 
-	//a member tasks that compares the user input with the correct answer
 	@Override
-	public boolean checkAnswer(long userAnswer, int articleQuestion) 
+	public boolean checkAnswer(String userAnswer, int articleQuestion) 
 	{
-		JLabel question = new JLabel("Correct Answer:");
-		return false;
+		int decimalNumber = articleQuestion;
+		int remainder;
+		String binary = "";
+				
+		while(decimalNumber > 0)
+		{
+			remainder = decimalNumber % 2;
+			binary = remainder + binary;
+			decimalNumber /= 2;
+		}
+		
+		if(userAnswer != binary)
+			return false;
+		return true;
 	}
 
-	//a directive to continue on in the program or to return to the previous tutorial
 	@Override
 	public void subsequentAction() 
 	{
 		// TODO Auto-generated method stub
 		
-	} 
+	}	
 	
 	//initates the arraylist value with random decimal numbers
-	public void initiateValueArray()
+	public int initiateConvertValue()
 	{
-		values.clear();
-		
 		Random rand = new Random();
-		for(int count = 0; count < practiceValues; count++)
-		{
-			int randomNum = rand.nextInt(1000);
-			values.add(randomNum);
-		}
+		int chooseArticleQuestion = rand.nextInt(1000);
 		
-		
+		return chooseArticleQuestion;			
 	}
 	
-	//converts the values in arraylist value into binary representations for the user to determine
-	public void initiateSolutionArray()
-	{
-		String binary = "";
-		int decimalNumber = 0;
-		int remainder = 0;
-		
-		for(int count =0; count < practiceValues; count++)
+	public DecimalToBinaryPractice()
+	{		
+		showQuestion(1);
+			    
+	    correctAnswer = checkAnswer(userResponse, convertValue);
+	    		
+		if(correctAnswer)
 		{
-			if(values.get(count) == 0)
-				solutions.add(count, 0);
-			else
-			{
-				
+			successLabel = new JLabel("Good Job! Proceed to the next tutorial!");
+				successLabel.setForeground(Color.WHITE);
+				successLabel.setVerticalTextPosition(JLabel.BOTTOM);
+				successLabel.setHorizontalTextPosition(JLabel.CENTER);
 			
-				while(decimalNumber > 0)
-				{
-					remainder = decimalNumber % 2;
-					binary = remainder + binary;
-					decimalNumber /= 2;
-				}
+			nextTutorialButton = new JButton("Next Tutorial!");
+				nextTutorialButton.setVerticalTextPosition(AbstractButton.CENTER);
+				nextTutorialButton.setHorizontalTextPosition(AbstractButton.LEADING);
+				nextTutorialButton.setLocation(200, 100);
+				nextTutorialButton.setToolTipText("Click this button to proceed to next tutorial.");
+				nextTutorialButton.setActionCommand("Success");
+				nextTutorialButton.addActionListener(this);
 				
-			}
-			int convertInt = Integer.parseInt(binary);
-			solutions.add(count, convertInt);
+				add(nextTutorialButton);
 		}
+		
+		else if(!correctAnswer)
+		{
+			failureLabel = new JLabel("Oops! You need more practice. Go back to the previous tutorial!");
+				failureLabel.setForeground(Color.WHITE);
+				failureLabel.setVerticalTextPosition(JLabel.BOTTOM);
+				failureLabel.setHorizontalTextPosition(JLabel.CENTER);
+			
+			previousTutorialButton = new JButton("Back to Tutorial!");
+				previousTutorialButton.setVerticalTextPosition(AbstractButton.BOTTOM);
+				previousTutorialButton.setHorizontalTextPosition(AbstractButton.CENTER);
+				//nextTutorialButton.setLocation(200, 100);
+				previousTutorialButton.setToolTipText("Click this button to proceed to previous tutorial.");
+				previousTutorialButton.setActionCommand("Failure"); 
+				previousTutorialButton.addActionListener(this);
+				
+				add(previousTutorialButton);
+		}
+	       
+	}
+	
+	public static void initPracticeProblem()
+	{
+		//JPanel practicePanel = new JPanel();
+		//JLabel practiceLabel = new JLabel("Decimal to Binary Practice Problem.");
+		
+		JFrame frame = new JFrame("Decimal to Binary Practice Problem.");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		DecimalToBinaryPractice pane = new DecimalToBinaryPractice();
+		pane.setBackground(Color.BLACK);
+		//
+		pane.setOpaque(true);
+		pane.setPreferredSize(new Dimension(600, 500));
+		
+		//frame.setBackground(Color.BLUE);
+		frame.add(pane);
+		frame.setSize(500, 500);
+		frame.pack();
+		frame.setVisible(true);
 		
 	}
 	
-	public void init()
+	public void paint(Graphics g)
 	{
-		try
+		super.paint(g);
+	}
+	
+	public static void main(String[] args)
+	{
+		javax.swing.SwingUtilities.invokeLater(new Runnable()
 		{
-			SwingUtilities.invokeAndWait(new Runnable()
+			public void run()
 			{
-					public void run()
-					{		
-						JPanel panel = new JPanel();
-						panel.setLayout(new BorderLayout());
-					//	panel.setName("Practice Calculating Decimal to Binary");
-					
-						JLabel lbl = new JLabel ("Practice Calculating Decimal to Binary");
-						
-						lbl.setFont(new Font("Verdana",23,30));				
-						
-						lbl.setLocation(0, 20);
-						lbl.setSize(86, 14);
-						lbl.setBorder(new LineBorder(Color.BLUE));
-						
-						lbl.add(panel, BorderLayout.NORTH);
-						add(lbl);
-					}
+				initPracticeProblem();
+			}
 		});
-			
-		}catch (Exception e)
-			{
-				System.err.println("createGUI didn't complete successfully");
-			}
-			
 		
 	}
 
-
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		if("Success".equals(e.getActionCommand()))
+		{
+			//go to next tutorial
+		}
+		
+		else if("Failure".equals(e.getActionCommand()))
+		{
+			//go to previous tutorial
+		}
+		
+		else if("Submitted".equals(e.getActionCommand()))
+		{
+			userResponse = editTextArea.getText();
+			
+		}		
+		
+	}
 	
-
-
 }
